84662bb8a5b1ab2b563724b35126b4b6e15dbbcf
ovsrinu <ovsrinu@gmail.com>
2024-09-03 09:49:09 +0530

    added views

---
+++ src/main/java/com/ovsit/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.ovsit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page")
+public class PageController {
+
+	@GetMapping("/home")
+	public String index(Model model) {
+		model.addAttribute("message", "Hello from AWS EC2");
+		return "Index";
+	}
+}
